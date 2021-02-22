e9b59e8e054bf8f9dbef89157766b9c64d9a19c1
Alexandre <78553030+StormLx@users.noreply.github.com>
2021-02-22 13:52:54 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -200,21 +200,7 @@
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
+  
   });
 
 })()
